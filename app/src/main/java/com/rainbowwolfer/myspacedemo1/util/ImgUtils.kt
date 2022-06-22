1d62ea2f2951513e682f1bce462f2d151cd109b6
Elizabevil <2197377595@qq.com>
2022-06-22 03:50:03 -0400

    ImgUtils

--- app/src/main/java/com/rainbowwolfer/myspacedemo1/util/ImgUtils.kt
+++ app/src/main/java/com/rainbowwolfer/myspacedemo1/util/ImgUtils.kt
@@ -3,12 +3,13 @@ package com.rainbowwolfer.myspacedemo1.util
 import android.content.Context
 import android.graphics.Bitmap
 import android.os.Environment
-import android.util.Log
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.rainbowwolfer.myspacedemo1.R
 import com.squareup.picasso.Picasso
-import java.io.*
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,6 +24,12 @@ object ImgUtils {
             .into(target)
     }
 
+    /**
+     * 判断是否拥有权限
+     * PermissionUtils.isGetPermissions(ct,"android.permission.WRITE_EXTERNAL_STORAGE" )
+
+     * 默认当期时间作 文件名
+     */
     fun saveBitmap(bitmap: Bitmap, filePath: String? = null) {
         var filename1: String =
             SimpleDateFormat("yyyyMMddHHmmss").format(Date(System.currentTimeMillis()))
@@ -32,15 +39,22 @@ object ImgUtils {
         }
         val file: File =
             File(Environment.getExternalStorageDirectory(), "$filename1.png")
-        saveBitmapFile(file, bitmap)
+        saveBitmapFile(bitmap, file)
     }
 
-    fun saveBitmapFile(file: File, bitmap: Bitmap) {
+    /**
+     * Save bitmap file
+     *
+     * @param file File对象
+     * @param bitmap Bitmap
+     */
+    fun saveBitmapFile(bitmap: Bitmap, file: File) {
+
+
         var bos: BufferedOutputStream = BufferedOutputStream(FileOutputStream(file));
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
         bos.flush();
         bos.close();
-
     }
 
 }
