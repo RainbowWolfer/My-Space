7ba5d79f2b74795a0052f0fd841a00cb3975bf8e
Elizabevil <2197377595@qq.com>
2022-06-22 02:16:47 -0400

    imgUtils

--- app/src/main/java/com/rainbowwolfer/myspacedemo1/util/ImgDownload.kt
+++ app/src/main/java/com/rainbowwolfer/myspacedemo1/util/ImgDownload.kt
@@ -1,62 +1,101 @@
 package com.rainbowwolfer.myspacedemo1.util
 
-import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Rect
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavOptions
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.google.android.material.textfield.TextInputLayout
-import com.google.gson.Gson
 import com.rainbowwolfer.myspacedemo1.R
-import com.rainbowwolfer.myspacedemo1.models.UserInfo
-import com.rainbowwolfer.myspacedemo1.models.UserInfo.Companion.findUserInfo
-import com.rainbowwolfer.myspacedemo1.models.api.GoResponse
-import com.rainbowwolfer.myspacedemo1.models.exceptions.ResponseException
-import com.rainbowwolfer.myspacedemo1.models.interfaces.DatabaseID
-import com.rainbowwolfer.myspacedemo1.services.application.MySpaceApplication
-import com.rainbowwolfer.myspacedemo1.services.callbacks.ArgsCallBack
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.joda.time.DateTime
-import org.joda.time.Days
-import org.joda.time.Minutes
-import org.json.JSONObject
-import retrofit2.Response
-import java.io.ByteArrayOutputStream
-import java.io.InputStream
-import java.math.BigInteger
-import java.security.MessageDigest
-import kotlin.random.Random
+import com.squareup.picasso.Picasso
+import java.io.File
 
+/*
+* 采用 Glide 与 picasso
+*/
 object ImgDownload {
-    /*
-    需要网络权限
-    * <uses-permission android:name="android.permission.INTERNET" />
-    Glide支持网络资源、assets资源、Resources资源、File资源、Uri资源、字节数组
-    * */
-    fun sdas(context: Context, bitmap: Bitmap, targetView: ImageView) {
-//		和Activity/Fragment的生命周期保持一致
-        /*	Glide.with(Context context);
-            Glide.with(Activity activity);
-            Glide.with(FragmentActivity activity);
-            Glide.with(Fragment fragment);*/
+    private val picasso: Picasso by lazy {
+        Picasso.get()
+    }
+
+/*    Glide支持网络资源、assets资源、Resources资源、File资源、Uri资源、字节数组
+          Integer resourceId
+          Uri uri
+          String string
+          Drawable drawable
+          Bitmap bitmap
+                */
+    /**
+     * Set img to
+     * @param context 上下文,与其生命周期相关联
+     * @param bitmap 图片
+     * @param targetView 目标对象 ImageView
+     */
+    fun setImgTo(context: Context, bitmap: Bitmap, targetView: ImageView) {
+
         Glide.with(context)
             .load(bitmap)
-            .error(R.drawable.ic_img_error)
+            .placeholder(R.drawable.ic_img_error)//图片尚未加载出来 用于占位
+            .error(R.drawable.ic_img_error)//图片尚未加载失败 用于占位
             .into(targetView)
 
+    }
+
+    /*
+        load(Uri?)
+        load(File)
+        load(Int)
+        load(String?) */
+    /**
+     * Get bitmap
+     *
+     * @param imgUrl 图片链接
+     * @param targetView 目标对象
+     * @return 获取 Bitmap
+     */
+    fun getBitmap(imgUrl: String, targetView: ImageView): Bitmap {
+
+        return picasso
+            .load(imgUrl)
+            .error(R.drawable.ic_img_error)
+            .get()
+    }
+
+
+    /*
+       load(Uri?)
+       load(File)
+       load(Int)
+       load(String?) */
+    /**
+     * Set img to
+     *
+     * @param imgUrl Uri File  Int String
+     * @param targetView
+     */
+    private fun setImgTo(imgUrl: Any, targetView: ImageView) {
+        when (imgUrl) {
+            is File -> picasso
+                .load(imgUrl)
+                .error(R.drawable.ic_img_error)
+                .into(targetView)
+            is String -> picasso
+                .load(imgUrl)
+                .error(R.drawable.ic_img_error)
+                .into(targetView)
+            is Uri -> picasso
+                .load(imgUrl)
+                .error(R.drawable.ic_img_error)
+                .into(targetView)
+            is Int -> picasso
+                .load(imgUrl)
+                .error(R.drawable.ic_img_error)
+                .into(targetView)
+            else ->
+                picasso.load(R.drawable.ic_img_error)
+                    .error(R.drawable.ic_img_error)
+                    .into(targetView)
+        }
     }
 
 
