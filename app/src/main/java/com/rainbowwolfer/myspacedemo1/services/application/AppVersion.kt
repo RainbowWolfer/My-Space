f1f8f4ca79038b6839377c6c09b73eb2ad454344
RainbowWolfer <RainbowWolfer@outlook.com>
2022-06-24 03:40:29 +0800

    Update AppVersion.kt

--- app/src/main/java/com/rainbowwolfer/myspacedemo1/services/application/AppVersion.kt
+++ app/src/main/java/com/rainbowwolfer/myspacedemo1/services/application/AppVersion.kt
@@ -13,9 +13,9 @@ class AppVersion(
 				return null
 			}
 			val marjor = split[0].toIntOrNull() ?: return null
-			val minor = split[0].toIntOrNull() ?: return null
-			val build = split[0].toIntOrNull() ?: return null
-			val revision = split[0].toIntOrNull() ?: return null
+			val minor = split[1].toIntOrNull() ?: return null
+			val build = split[2].toIntOrNull() ?: return null
+			val revision = split[3].toIntOrNull() ?: return null
 			
 			return AppVersion(marjor, minor, build, revision)
 		}
